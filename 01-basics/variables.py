f5e657153e166ce4ad56a0a39d1c9f07892ada40
Wayne <olympia5k@yahoo.com>
2026-07-05 16:20:45 -0400

    Practice variable reassignment and multiple assignment

--- 01-basics/variables.py
+++ 01-basics/variables.py
@@ -1,5 +1,34 @@
 # Lecture 27: Variables
-name = "Wayne"
-age = 42
-print(name)
-print(age)
+#name = "Wayne"
+#age = 42
+#print(name)
+#print(age)
+
+
+
+iq = 190
+
+user_age = iq/4
+
+a = user_age 
+
+print(a)
+
+
+
+#constants
+
+
+PI = 3.14
+PI = 0
+
+print(PI)
+
+
+
+
+a,b,c = 1,2,3
+
+print(a)
+print(b)
+print(c)
