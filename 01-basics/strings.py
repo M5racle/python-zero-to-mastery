dec62185d53bfbaab28581b3335727447ecf8bf9
Wayne <olympia5k@yahoo.com>
2026-07-05 17:43:02 -0400

    Practice string variables and print statements (Lecture 30)

---
+++ 01-basics/strings.py
@@ -0,0 +1,27 @@
+# Lecture 30: Stringstype
+
+#print(type('Hi, hello there'))
+#user_name = 'Wayne'
+#password = 'supersecret'
+#long_string = '''
+#WOW
+#0 0
+#---
+#'''
+#print(long_string)
+
+#first_name = 'Wayne'
+#last_name = 'Lumpkin'
+#full_name = first_name + ' ' + last_name
+#print(full_name)
+
+
+
+
+
+
+
+
+
+
+
