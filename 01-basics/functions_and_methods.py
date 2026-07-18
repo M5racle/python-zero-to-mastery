2feacd04447cebf904d98762013a879daed20518
Wayne <olympia5k@yahoo.com>
2026-07-17 20:02:53 -0400

    Completed Lecture #40.

--- 01-basics/functions_and_methods.py
+++ 01-basics/functions_and_methods.py
@@ -60,7 +60,13 @@
 # the overall purpose of a complex function, algorithm, or file logic.
 
 
+#Exercise Password Checker
 
+#username = input('What is your username?')
+#password = input('What is your password?')
+#password_length = len(password)
+#hidden_password = '*' * password_length
+#print(f'{username}, your password, {hidden_password}, is {password_length} letters long')
 
 
 
